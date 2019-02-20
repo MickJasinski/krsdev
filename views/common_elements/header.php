56db79b0234355bfa012d96de5bfd9fa5f1b2899
Michal Jasinski <gaap@Michals-MacBook-Pro.local>
2019-02-20 01:48:16 +0000

    General build 1

--- views/common_elements/header.php
+++ views/common_elements/header.php
@@ -1,3 +1,27 @@
-<?php 
-
-echo 'header';
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Serwis Komputerowy Bedford</title>
+  <meta name="description" content="Serwis komputerów, laptopów, tabletów Bedford">  
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+  <!-- End of head -->
+<header class="dark-bg container-fluid">
+  <div class="row">
+    <div class="col-6 logo-wrap">
+      <img class="logo" src="images/logoServ.svg" alt="Logo - Serwis Komputerowy Bedford">       
+    </div>
+    <div class="col-6">
+      <ul>
+        <li><a class="nav-link" href="/">Start</a></li>
+        <li><a class="nav-link" href="/o-nas">O nas</a></li>
+        <li><a class="nav-link" href="/kontakt">Kontakt</a></li>
+      </ul>
+    </div>
+  </div>
+</header>
