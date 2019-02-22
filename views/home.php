b701f1b0b5f6e44bb8aa677fc723ad6c6367d023
Michal Jasinski <gaap@Michals-MacBook-Pro.local>
2019-02-22 00:13:20 +0000

    Hero works 02

--- views/home.php
+++ views/home.php
@@ -1,15 +1,26 @@
 <?php require 'common_elements/header.php'; ?>
 
-<section id="home" class="dark-lite-blue">
-  <div id="HomeArtworkWrap">
-    <h1 id="BannerTxt">Rozwiążemy każdy problem z Twoim</h1>
-    <div class="slidingVertical">
-      <span id="AnimateTxt">Komputerem</span>
-      <span id="AnimateTxt">Tabletem</span>
-      <span id="AnimateTxt">Smartfonem</span>
+<section id="home" class="dark-lite-blue container-fluid">
+  <div class="row">
+    <div class="callme col-xs-12 col-md-6">
+      <h1 id="BannerTxt">Rozwiążemy każdy problem z Twoim</h1>
+
     </div>
-    <img id="HomeArtwork" src="images/homeArtwork.svg" />
+
+    <div class="artwork col-xs-12 col-md-6">      
+      <div id="HomeArtworkWrap">
+        <div class="sliding-vertical">
+          <span class="animated-text">Komputerem</span>
+          <span class="animated-text">Tabletem</span>
+          <span class="animated-text">Smartfonem</span>
+        </div>
+        <img id="HomeArtwork" src="images/homeArtwork.svg" />
+      </div>
+    </div>
   </div>
+
+
+
 </section>
 
 <?php require 'common_elements/footer.php'; ?>  
