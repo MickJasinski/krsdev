ebb58ae6cd99f1351dab552fada877fc318caa48
Michal Jasinski <gaap@Michals-MacBook-Pro.local>
2019-02-25 23:59:28 +0000

    Artwork works again.

--- views/home.php
+++ views/home.php
@@ -1,26 +1,22 @@
 <?php require 'common_elements/header.php'; ?>
-
 <section id="home" class="dark-lite-blue container-fluid">
-  <div class="row">
-    <div class="callme col-xs-12 col-md-6">
-      <h1 id="BannerTxt">Rozwiążemy każdy problem z Twoim</h1>
-
-    </div>
-
-    <div class="artwork col-xs-12 col-md-6">      
-      <div id="HomeArtworkWrap">
+  <div class="background"></div>
+  <div class="row row-reset">
+    <div class="artwork col-12">     
+      <div class="artwork-wrap">
         <div class="sliding-vertical">
+            <h1 id="banner-text">Rozwiążemy każdy <br> problem z Twoim</h1>
           <span class="animated-text">Komputerem</span>
           <span class="animated-text">Tabletem</span>
           <span class="animated-text">Smartfonem</span>
         </div>
         <img id="HomeArtwork" src="images/homeArtwork.svg" />
-      </div>
+      </div> 
     </div>
   </div>
-
-
-
 </section>
+<a class="callme" href="/kontakt">
+  <button><p>Zgłoś problem</p></button>
+</a>
 
 <?php require 'common_elements/footer.php'; ?>  
